d6d3f277487443df385912d7194df0db0063cad9
Ahmadjajja <ahmadjajja86@gmail.com>
2024-03-10 11:10:25 +0500

    JS 1-Class22

---
+++ 4-Javascript/1-Class22/script.js
@@ -0,0 +1,38 @@
+// we can declare a variable with 3 types
+// 1- let   2- var  3- const
+
+// what is variable?
+// variable is just like a container that stores data of multiple types
+
+//      Data types -> integers number, floating number, character, string, boolean, and
+//                    list of all above data types -> arrays, object
+
+let num1 = 2;
+
+for (let index = 1; index > 0; index++) {
+
+        if (index != 5) {
+
+            console.log(`${num1} * ${index} = ${num1 * index}`)
+
+        }
+
+}
+
+
+
+
+
+
+
+
+// 2 * 1 = 2
+// 2 * 2 = 4
+// 2 * 3 = 6
+// 2 * 4 = 8
+
+// 2 * 6 = 12
+// 2 * 7 = 14
+// 2 * 8 = 16
+// 2 * 9 = 18
+// 2 * 10= 20
