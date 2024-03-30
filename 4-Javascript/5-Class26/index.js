f393e32785295f3bb117517f01690002970fb5fe
Ahmadjajja <ahmadjajja86@gmail.com>
2024-03-30 23:05:08 +0500

    5-Class26

---
+++ 4-Javascript/5-Class26/index.js
@@ -0,0 +1,63 @@
+// var firstName = prompt("Enter your first name : ", "Ahmad");
+// var FirstName = prompt("Enter your first name : ", "Ahmad");
+// var first_name = prompt("Enter your first name : ", "Ahmad");
+
+// var secondName = prompt("Enter your second name : ", "Sultan");
+
+
+// function greet(fName, sName) {
+//     console.log("Hello "+ fName + " "+ sName)  // Hello ahmad sultan
+//     console.log(`Hello ${fName} ${sName}`)  // Hello ahmad sultan
+
+// }
+
+
+// greet(firstName, secondName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var nums = [1, "ahmad", false,4]
+console.log(nums);
+console.log(nums[0]);
+console.log(nums[1]);
+console.log(nums[2]);
+console.log(nums[3]);
+
+for (let index = 0; index < nums.length; index++) {
+    console.log("element at index ", index, " -> ", nums[index])
+    
+}
+
+
+
+
