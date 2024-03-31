91130bd63d9558d8ce74c128ff2a634bb2fc28ea
Ahmadjajja <ahmadjajja86@gmail.com>
2024-03-31 12:53:33 +0500

    6-Class27 ass-1

---
+++ 4-Javascript/6-Class27 ass-1/index.js
@@ -0,0 +1,27 @@
+// Q1
+// 1. Write a js program to find the maximum between two numbers.
+
+let price1 = prompt("Enter price 1: ")
+let price2 = prompt("Enter price 2: ")
+
+if(price2 > price1) {
+
+    console.log(`${price2} is greater than ${price1}`);
+    alert(`${price2} is greater than ${price1}`)
+
+}
+
+if (price1 > price2) {
+
+    console.log(`${price1} is greater than ${price2}`);
+    alert(`${price1} is greater than ${price2}`)
+
+} 
+
+
+if(price1 == price2){
+
+    console.log(`${price1} is equal to ${price2}`);
+    alert(`${price1} is equal to ${price2}`)
+
+}
