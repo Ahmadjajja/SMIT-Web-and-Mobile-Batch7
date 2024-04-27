40f4ab089921e20dfd78af933c82890a34b008fb
Ahmadjajja <ahmadjajja86@gmail.com>
2024-04-28 00:04:40 +0500

    10-Class31/

---
+++ 4-Javascript/10-Class31/index.js
@@ -0,0 +1,81 @@
+// Write JavaScript functions to solve the following string problems
+
+// // a) Return the length of a given string.
+
+// function calculateLength(userName){
+//     return userName.length
+// }
+
+// var name1 = "shahzad"
+// var name2 = "moeez zafar"
+// var name3 = "zubair don"
+
+// console.log(calculateLength(name1));
+// console.log(calculateLength(name2));
+// console.log(calculateLength(name3));
+
+
+
+
+// // b) Concatenate two strings together.
+
+// var firstName = "Ahmad"
+// var secondName = "Ali"
+// console.log(firstName +" "+ secondName);
+
+// // c) Determine if a given string is empty.
+
+// function checkString(str) {
+//     if (!str.trim()) {
+//         return "Empty String"
+//     } else {
+//         return "No Empty String"
+//     }
+    
+// }
+
+// console.log(checkString("Ahmad"))
+
+
+
+// // d) Count the number of vowels in a string.    "Ahmad" -> "ahmad"
+
+// function countVowels(str) {
+//     let count = 0
+//     str = str.toLowerCase()
+
+//     for (let index = 0; index < str.length; index++) {
+//         let character = str.charAt(index);
+//         if (character == "a" ||character == "e" ||character == "i" ||character == "o" ||character == "u") {
+//             count++
+//         }
+//     }
+    
+
+//     return count
+// }
+
+// console.log(countVowels("Ahmad"));
+
+
+
+
+
+
+
+// e) Reverse a given string.
+// f) Check if a string is a palindrome.
+// g) Convert a string to uppercase.
+// h) Find the first occurrence of a given character in a string.
+// i) Replace all occurrences of a given character in a string with another character.
+// j) Trim leading and trailing whitespace from a string.
+// k) Count the number of words in a string.
+// l) Check if a string contains only numeric characters.
+// m) Check if a string is a valid email address.
+// n) Extract the domain name from a URL string.
+// o) Convert a string into title case (the first letter of each word capitalized).
+// p) Remove all non-alphabetic characters from a string.
+// q) Check if a string is a valid palindrome permutation (ignoring spaces).
+// r) Check if a string is an anagram of another string.
+// s) Find the longest word in a string.
+// t) Perform string compression by replacing repeated characters with the character followed by the count.
---
+++ 4-Javascript/11-Class32/index.js
@@ -0,0 +1,79 @@
+
+
+
+
+
+let person = {
+    name: "Ahmad",
+    education: ["BSCS", "SMIT Certified"],
+    ID: "765432",
+    run: function () {
+        return "Ahmad is running."
+    }
+}
+
+
+// console.log("person -> ", person)
+// console.log("person.name -> ", person.name)
+// console.log("person.education -> ", person.education)
+// console.log("person.ID -> ", person.ID)
+// console.log("person.run -> ", person.run())
+
+// console.log(Object.values(person))
+// console.log(Object.keys(person))
+
+// let classData = [{
+//     name: "Ahmad",
+//     education: "BSCS",
+//     ID: "765432",
+//     run: function () {
+//         return "Ahmad is running."
+//     }
+// }, {
+//     name: "aqib",
+//     education: "BSCS",
+//     ID: "765432",
+//     run: function () {
+//         return "Ahmad is running."
+//     }
+// }, {
+//     name: "Rehman",
+//     education: "BSCS",
+//     ID: "765432",
+//     run: function () {
+//         return "Ahmad is running."
+//     }
+// }, {
+//         name: "ALi",
+//         education: "BSCS",
+//         ID: "765432",
+//         run: function () {
+//             return "Ahmad is running."
+//         }
+//     }]
+
+
+//     for (let index = 0; index < classData.length; index++) {
+
+//         if (classData[index].name == "aqib") {
+//             console.log(classData[index])
+//         }
+
+//     }
+
+
+
+// for in
+
+for(const property in person){
+
+    console.log(property)// for getting keys
+    if (property == "run") {
+        console.log(person[property]())// for calling run function
+    } else {
+        console.log(person[property])// for getting values
+    }
+    console.log("...............")
+
+}
+
