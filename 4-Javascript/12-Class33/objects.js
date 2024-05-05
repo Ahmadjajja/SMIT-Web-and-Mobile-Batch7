b7403e7ef9a119ccb79563b5a0cc7e32b0035794
Ahmadjajja <ahmadjajja86@gmail.com>
2024-05-05 10:54:44 +0500

    12-Class33

---
+++ 4-Javascript/12-Class33/objects.js
@@ -0,0 +1,146 @@
+/*
+attributes
+
+name
+height
+age
+id
+hair color
+skin color eye color
+personality
+
+functions
+
+running
+walking
+talking
+hearing
+eating
+sleeping
+
+*/
+
+
+// const person = {
+//     fName:"Ali",
+//     ID: "2345902034759023475",
+//     age: 22,
+//     run: function() {
+//         return "He runs very fast."
+//     }
+// }
+
+
+// console.log(
+//     person.fName, person["fName"], Object.keys(person), Object.values(person)
+// )
+
+
+// let arr = []
+
+// for (const key in person) {
+//     console.log(person[key])
+// }
+
+// ==============================================================
+
+// map, filter, find
+
+// // map
+// let nums = [2, 3, 4, 6, 333, 4,6]
+// console.log("NUMS before MAP: ", nums)
+// let nums2 = nums.map(function(num) {
+//     if(num < 50) {
+//         num = num * 3
+//     } else {
+//         num = num * 5
+//     }
+//     return num
+// })
+// // [6, 9, 12, 18, ____, 12, 18]
+
+// console.log("NUMS after MAP: ", nums2)
+
+// // filter
+// let students = [
+//     { nam: "ali", marks: 90 },
+//     { nam: "ahmad", marks: 70 },
+//     { nam: "hamza", marks: 99 },
+//     { nam: "rehman", marks: 40 },
+//     { nam: "babar", marks: 93 }
+// ]
+
+// console.log("All students -> ", students)
+
+// let brilliantStudents = students.filter(function (student) {
+//     return student.marks > 90
+// })
+
+// console.log("Brilliant students -> ", brilliantStudents)
+
+
+
+
+// // find 
+// let students = [
+//     { nam: "ali", marks: 90 },
+//     { nam: "ahmad", marks: 70 },
+//     { nam: "hamza", marks: 99 },
+//     { nam: "rehman", marks: 40 },
+//     { nam: "hamza", marks: 98 },
+//     { nam: "babar", marks: 93 }
+// ]
+
+// console.log("All students -> ", students)
+
+// let brilliantStudent = students.find(function (student) {
+//     return student.marks > 95
+// })
+
+// console.log("Brilliant student -> ", brilliantStudent)
+
+
+// break, continue
+
+// let x2 = 100;
+// for (let index = 0; index < 100; index++) {
+//     let x = 55;
+//     if (index > 5 && index < 99) {
+//         continue
+//     }
+//     console.log(index);
+//     console.log(index);
+//     console.log(index);
+//     console.log(index);
+//     console.log(index);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
