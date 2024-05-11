092606d642becc740e12b5aa28a5392ad32e3d3d
Ahmadjajja <ahmadjajja86@gmail.com>
2024-05-11 12:32:44 +0500

    13-Class34

---
+++ 4-Javascript/13-Class34/index.js
@@ -0,0 +1,22 @@
+let num = [2, 4, 7, 8]
+
+console.log(num.includes(70));
+console.log(num.findIndex(item => item == 7));
+
+
+// // for (let index = 0; index < num.length; index++) {
+// //     num[index] = 1
+// // }
+
+// // num.fill(1)
+
+// num.reverse()
+// console.log(num);
+
+// let x = 2;
+// function doubler() {
+//     x = x * 2
+//     return x
+// }
+// console.log(doubler())
+// console.log(x)
