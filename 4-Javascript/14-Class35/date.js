602ca1814727f748a54106b39a8192ff2ef34f9a
Ahmadjajja <ahmadjajja86@gmail.com>
2024-05-12 12:54:44 +0500

    14-Class35

---
+++ 4-Javascript/14-Class35/date.js
@@ -0,0 +1,14 @@
+// let date = new Date("jan 10, 2024");
+
+
+// let days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
+
+// console.log("day -> ", days[date.getDay()])
+// console.log("hours -> ", date.getMinutes())
+
+
+// let date = new Date("jan 10, 2024");
+let date = new Date();
+console.log("now -> ", date)
+date.setFullYear(2004)
+console.log("2004 -> ", date)
---
+++ 4-Javascript/14-Class35/index.js
@@ -0,0 +1,19 @@
+// let randomNumber = Math.random()
+// let die = (Math.floor((randomNumber * 6) + 1))
+
+// console.log(die)
+
+
+// let nam = prompt("Enter your age: ")// type of nam is string
+// console.log(nam) 
+
+// // nam = parseInt(nam)
+// nam = parseFloat(nam)
+
+// console.log(typeof nam)
+// console.log(nam)
+// // console.log(typeof nam)
+
+
+console.log(150 * "200")
+
