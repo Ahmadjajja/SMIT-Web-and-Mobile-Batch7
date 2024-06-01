6737ac4bd397d5f60511c78041f96a521d59e76e
Ahmadjajja <ahmadjajja86@gmail.com>
2024-06-01 10:57:23 +0500

    19-Class40

---
+++ 4-Javascript/19-Class40/script.js
@@ -0,0 +1,31 @@
+
+// // get element by id
+
+// console.log(document.getElementById("p1"))
+// document.getElementById("p1").style.backgroundColor = "green"
+// document.getElementById("p1").style.color = "yellow"
+// document.getElementById("p1").className += "paragraph"
+// console.log(document.getElementById("p1"))
+
+// get element by tag name
+
+
+// let allPTags = document.getElementsByTagName("p")
+// for (let index = 0; index < allPTags.length; index++) {
+//     allPTags[index].style.color = "red"
+//     allPTags[index].style.backgroundColor = "black"
+// }
+
+
+
+let document = {
+    html: {
+        head: {
+            title: "my title"
+        }, 
+        body: {
+            a: "My link",
+            h1: "My header"
+        }
+    }
+}
