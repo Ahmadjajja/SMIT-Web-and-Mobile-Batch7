4773a0f42ffa4650aa20a111f23864289354a64b
Ahmadjajja <ahmadjajja86@gmail.com>
2024-06-23 10:52:42 +0500

    Class45

---
+++ 4-Javascript/25-Class45/browser_control.js
@@ -0,0 +1,7 @@
+function checkURL() {
+    console.log(window.location.href)
+    console.log(window.location.hostname)
+    console.log(window.location.pathname)
+    // window.location.replace("https://github.com/Ahmadjajja")
+    history.go(2)
+}
