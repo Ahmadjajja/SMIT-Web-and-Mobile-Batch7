55d017ae0dbb25fef0fccbe69afe910da3fa2129
Ahmadjajja <ahmadjajja86@gmail.com>
2024-06-29 10:50:21 +0500

    Class46

---
+++ 4-Javascript/26-Class46/try_catch.js
@@ -0,0 +1,34 @@
+let num = 5
+num;
+num;
+num;
+num;
+num;
+num;
+num;
+num;
+
+
+
+try {
+
+    num;
+    num;
+    throw "somethings wrong!"
+    num;
+    console.log("Hello Everyone!")
+    
+} catch (error) {
+
+    console.log("Error happened: ", error)
+    
+}
+
+console.log("asdfasdf")
+
+num;
+num;
+num;
+num;
+num;
+num;
