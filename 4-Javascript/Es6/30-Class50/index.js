201305bdee18f2c57adc014e47cbfda9ebcc7777
Ahmadjajja <ahmadjajja86@gmail.com>
2024-07-13 14:08:41 +0500

    30-Class50

---
+++ 4-Javascript/Es6/30-Class50/index.js
@@ -0,0 +1,23 @@
+// let names = [1, 4, 5, 6, 8]
+
+
+// let [name1,, name3, ...nums] = names
+
+// console.log(name1, name3, nums, x)
+
+// let person = {
+//     name: "Ahmad",
+//     age: "23",
+//     qualification: "graduate"
+// }
+
+
+// let {name, qualification} = person
+
+// console.log(name, qualification)
+
+import nums from "./main.js"
+
+let sumOfItself = (n1, n2) => n1 + n2
+
+console.log(sumOfItself(nums.num1, nums.num2))
---
+++ 4-Javascript/Es6/30-Class50/main.js
@@ -0,0 +1,4 @@
+const num1 = 50
+const num2 = 40
+
+export default { num1, num2 }
